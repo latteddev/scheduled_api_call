342b1caed162266bf14a6724813bfe6474b08429
latteddev <laurenzcarlo.rk@gmail.com>
2026-09-16 11:29:38 +0800

    Created a basic API call to https://jsonplaceholder.typicode.com/posts

---
+++ main.py
@@ -0,0 +1,55 @@
+import json
+
+import requests
+
+
+def get_posts():
+
+    url = 'https://jsonplaceholder.typicode.com/posts'
+
+    try:
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            posts = response.json()
+            return posts
+
+        else:
+            print('Error:', response.status_code)
+            return None
+
+    except requests.exceptions.RequestException as e:
+
+        print('Error:', e)
+        return None
+
+
+def write_to_file(post):
+
+    with open("demofile.txt", "a") as f:
+        f.write(post)
+
+
+def main():
+    posts = get_posts()
+
+    if posts:
+        first_post = posts[0]
+        print(first_post)
+        print('Type:', type(first_post))        
+
+        try:
+            first_post = json.dumps(first_post, indent=4)
+            write_to_file(first_post)
+
+        except Exception as e:
+            return (e,'Failed to write to file')
+
+    else:
+        return 'No result from API'
+
+
+        
+if __name__ == '__main__':
+    main()
+    
